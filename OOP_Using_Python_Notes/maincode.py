52f54bf65ad66d7725b851fbfbf898a1b01ba816
vijay-guntireddy <vijay.guntireddy@live.com>
2019-01-04 14:10:34 +0530

    explained __main__

---
+++ OOP_Using_Python_Notes/maincode.py
@@ -0,0 +1,5 @@
+import testmodule 
+
+print(__name__)
+print(testmodule.name)
+print(testmodule.value)
---
+++ OOP_Using_Python_Notes/testmodule.py
@@ -0,0 +1,6 @@
+name = "aap"
+value = 10
+
+print("__name__ = {}".format(__name__))
+if __name__ == '__main__':
+   print("name = {},value = {}".format(name,value))
